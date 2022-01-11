ab33fe2d6f5cc5f7d9df3ee2fa8ffbdc75ad6e0b
profsheeku <97516847+profsheeku@users.noreply.github.com>
2022-01-11 15:57:04 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    int c = a+b;
+    cout<<"the sum of "<<a<<" and "<<b<<" is  " <<c<<endl;
+    
+    cout<<endl<<"-------------------------"<<endl;
+    cout<<"coding is fun"<<endl;
+
+    return 0;
+}
